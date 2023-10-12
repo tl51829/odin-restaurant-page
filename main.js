a52f4281f00d439bd17d5b47b928fb7873762d2d
tl51829 <135583181+tl51829@users.noreply.github.com>
2023-10-12 17:46:49 +0800

    Add navigation bar feature

--- main.js
+++ main.js
@@ -14,12 +14,49 @@ __webpack_require__.r(__webpack_exports__);
     const description = document.createElement("p");
 
     const container = document.querySelector("#content");
+    container.innerHTML = "";
 
     headline.textContent = "Welcome to Papa's Pizzeria";
     description.textContent = "Papa's Pizzeria was founded in 1869 by our great grandpapa Paparachi. Back in the days, Paparachi's cheese pizza was a hit and due to continuous innovation of his descendents, Paparachi's cheese pizza continue to amaze locals and foreigners alike. Don't believe us? Come and try Paparachi's homemade pizza!";
 
     container.appendChild(headline);
     container.appendChild(description);
+};
+
+/***/ }),
+/* 2 */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* export default binding */ __WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ function __WEBPACK_DEFAULT_EXPORT__() {
+    const container = document.querySelector("#content");
+    container.innerHTML = "";
+
+    const para = document.createElement("p");
+    para.textContent = "Menu!";
+
+    container.appendChild(para);
+};
+
+/***/ }),
+/* 3 */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* export default binding */ __WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ function __WEBPACK_DEFAULT_EXPORT__() {
+    const container = document.querySelector("#content");
+    container.innerHTML = "";
+
+    const para = document.createElement("p");
+    para.textContent = "Contact!";
+
+    container.appendChild(para);
 };
 
 /***/ })
@@ -83,10 +120,32 @@ var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _initial_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
+/* harmony import */ var _home_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
+/* harmony import */ var _menu_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(2);
+/* harmony import */ var _contact_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(3);
 
 
-(0,_initial_js__WEBPACK_IMPORTED_MODULE_0__["default"])();
+
+
+const header = document.querySelector('header');
+const list = document.createElement('ul');
+
+const tabs = ["Home", "Menu", "Contact"];
+for (let i = 0; i < 3; i++) {
+    const item = document.createElement('li');
+    item.textContent = tabs[i];
+    list.appendChild(item);
+
+    item.addEventListener("click", () => {
+        tabs[i] === "Home" ? (0,_home_js__WEBPACK_IMPORTED_MODULE_0__["default"])() :
+        tabs[i] === "Menu" ? (0,_menu_js__WEBPACK_IMPORTED_MODULE_1__["default"])() :
+        (0,_contact_js__WEBPACK_IMPORTED_MODULE_2__["default"])();
+    });
+}
+
+header.appendChild(list);
+
+(0,_home_js__WEBPACK_IMPORTED_MODULE_0__["default"])();
 })();
 
 /******/ })()
